24da9126c4c3ee9980729851107d7b9e451b5ca4
Shaurya Pratap Singh <151380293+shauryapratap1210@users.noreply.github.com>
2026-03-15 23:29:31 +0530

    Added Scope Practice program to understand its concept

---
+++ 05-Functions/Scopes.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Scopes {
+    public static void main(String[] args) {
+        // Here is error because its used before declartion System.out.println(i);
+        int i=20;
+        System.out.println(i); //here is used because after declaration
+        for(int r=1;r<=5;r++){
+            System.out.println(r);  //Block scope
+        }
+        System.out.println(r);  // r cant be used because its out of for loop block
+    }
+}
