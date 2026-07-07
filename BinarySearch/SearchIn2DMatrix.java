cf56ef911b9194065e3697fb0a24f84107082a9f
shauryapratap1210 <shauryapratapsinghs1210@gmail.com>
2026-07-07 13:29:57 +0530

    Optimized Approach for Search In 2D matrix

--- BinarySearch/SearchIn2DMatrix.java
+++ BinarySearch/SearchIn2DMatrix.java
@@ -3,14 +3,39 @@ package BinarySearch;
 class SearchIn2DMatrix {
     public static boolean searchMatrix(int[][] matrix, int target) {
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == target) {
-                    return true;
-                }
+        // for (int i = 0; i < matrix.length; i++) {
+        // for (int j = 0; j < matrix[i].length; j++) {
+        // if (matrix[i][j] == target) {
+        // return true;
+        // }
+        // }
+        // }
+
+        // return false;
+
+        // Binary Search
+        int totRow = matrix.length;
+        int totCol = matrix[0].length;
+
+        // 1D Array
+        int n = totRow * totCol;
+        int s = 0;
+        int e = n - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            int row = mid / totCol;
+            int col = mid % totCol;
+
+            if (matrix[row][col] == target) {
+                return true;
+            }
+
+            else if (matrix[row][col] < target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
         }
-
         return false;
 
     }
